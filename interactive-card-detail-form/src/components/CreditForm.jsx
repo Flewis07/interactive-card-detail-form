f9fbadaa97b73d922f32b6692a7c919eccf2f444
francis.lewis07@gmail.com <francis.lewis07@gmail.com>
2023-03-10 09:26:11 -0500

    Visual changes for form inputs

--- interactive-card-detail-form/src/components/CreditForm.jsx
+++ interactive-card-detail-form/src/components/CreditForm.jsx
@@ -5,23 +5,23 @@ export default function CreditForm() {
     return(
         <div className="credit-form">
             <form action="">
-                <div>
+                <div className="label-input">
                     <label htmlFor="">Cardholder name</label>
                     <input className="w-100 form-input" type="text" />
                 </div>
-                <div>
+                <div className="label-input">
                     <label htmlFor="">Card number</label>
-                    <input className="w-100 form-input" type="text" />
+                    <input className="w-100 form-input" type="text" placeholder="1234 5678 9123 0000"/>
                 </div>
                 <div className="d-flex">
-                    <div className="expiration">
+                    <div className="expiration label-input">
                         <label htmlFor="">Exp. Date (MM/YY)</label>
                         <div className="w-100">
                             <input className="w-50 form-input" type="text" />
                             <input className="w-50 form-input" type="text" />
                         </div>
                     </div>
-                    <div className="cvc w-50">
+                    <div className="cvc w-50 label-input">
                         <label htmlFor="">CVC</label>
                         <div>
                             <input className="w-100 form-input" type="text" />
