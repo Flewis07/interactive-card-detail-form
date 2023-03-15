7ae80c728d7b018ba4fd41d182b6b2b0492f2c04
Francis <ff291081@ens.uqam.ca>
2023-03-14 21:32:35 -0400

    Making sure that everything is responsive as it should be for mobile and desktop

--- interactive-card-detail-form/src/components/CreditForm.jsx
+++ interactive-card-detail-form/src/components/CreditForm.jsx
@@ -7,24 +7,24 @@ export default function CreditForm() {
             <form action="">
                 <div className="label-input">
                     <label htmlFor="">Cardholder name</label>
-                    <input className="w-100 form-input" type="text" />
+                    <input className="w-100 form-input full-width-input" type="text" placeholder="e.g. Jane Appleseed"/>
                 </div>
                 <div className="label-input">
                     <label htmlFor="">Card number</label>
-                    <input className="w-100 form-input" type="text" placeholder="1234 5678 9123 0000"/>
+                    <input className="w-100 form-input full-width-input" type="text" placeholder="e.g. 1234 5678 9123 0000"/>
                 </div>
-                <div className="d-flex">
-                    <div className="expiration label-input">
+                <div className="expirationcvc d-flex">
+                    <div className="expiration label-input half-width-input">
                         <label htmlFor="">Exp. Date (MM/YY)</label>
                         <div className="w-100">
-                            <input className="w-50 form-input" type="text" />
-                            <input className="w-50 form-input" type="text" />
+                            <input className="w-50 form-input" type="text" placeholder="MM"/>
+                            <input className="w-50 form-input" type="text" placeholder="YY"/>
                         </div>
                     </div>
                     <div className="cvc w-50 label-input">
                         <label htmlFor="">CVC</label>
                         <div>
-                            <input className="w-100 form-input" type="text" />
+                            <input className="w-100 form-input half-width-input" type="text" placeholder="e.g. 123"/>
                         </div>
                     </div>
                 </div>
